4f92be0bc109f6a426e6d221ebf55c9b2bf27958
kentah <kenthow@gmail.com>
2017-06-08 19:44:22 -0500

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const createUser = require('./createUser');
+const serviceAccount = require('service_account.json');
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://o-time-p.firebaseio.com"
+});
+
+exports.createUser = functions.https.onRequest(createUser);
