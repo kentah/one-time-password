2c2c06415ee60abc4044dfea2259bd981c3625b5
kentah <kenthow@gmail.com>
2017-06-10 14:03:18 -0500

    Added Twilio client

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./createUser');
-const serviceAccount = require('service_account.json');
+const serviceAccount = require('./service_account.json');
 
 
 admin.initializeApp({
